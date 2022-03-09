b71dba9e07967008f00b86dddc576a1c198f20e8
Linus Weigand <linusweigand@protonmail.com>
2022-03-09 14:07:44 +0100

    Adding Mock Environment

--- brownie_fund_me/scripts/deploy.py
+++ brownie_fund_me/scripts/deploy.py
@@ -1,10 +1,31 @@
-from brownie import FundMe
+from brownie import FundMe, network, config, MockV3Aggregator
 from scripts.helpful_scripts import get_account
 
 
 def deploy_fund_me():
     account = get_account()
-    fund_me = FundMe.deploy({"from": account})
+    # pass price feed address to our funme contract
+
+    # if we are on a persistent network like rinkeby, use the associated address
+    # otherwise, deploy mocks
+    if network.show_active() != "development":
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else:
+        print(f"The active network is {network.show_active()}")
+        print(f"Deploying Mocks...")
+        mock_aggregator = MockV3Aggregator.deploy(
+            18, 2000000000000000000000, {"from": account}
+        )
+        price_feed_address = mock_aggregator.address
+        print("Mocks deployed!")
+
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=True,
+    )
     print(f"Contract deployed to {fund_me.address}")
 
 
