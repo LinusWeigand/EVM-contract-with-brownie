f018b4566964cdd2e97e0cf83c91d184cc212a10
Linus Weigand <linusweigand@protonmail.com>
2022-03-09 14:59:36 +0100

    Error fixing

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -5,8 +5,9 @@ from scripts.helpful_scripts import (
 
 
 def deploy_fund_me():
+    print(f"Network is: {network.show_active()}")
     account = get_account()
-    if network.show_active():
+    if network.show_active() != "development":
         price_feed_address = config["networks"][network.show_active()][
             "eth_usd_price_feed"
         ]
@@ -22,7 +23,7 @@ def deploy_fund_me():
     fund_me = FundMe.deploy(
         price_feed_address,
         {"from": account},
-        publish_source=True,
+        publish_source=False,
     )
     print(f"Contract deployed to {fund_me.address}")
 
